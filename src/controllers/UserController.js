23b37609393c8b423662f95906ea3c94cc36cc6c
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-03-06 20:00:36 -0300

    :sparkles: feat: url params

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -13,5 +13,18 @@ module.exports = {
 
     response.writeHead(200, { 'content-type': 'application/json' });
     response.end(JSON.stringify(sortedUsers));
+  },
+  getUserById(request, response) {
+    const { id } = request.params;
+
+    const user = users.find(user => user.id === Number(id));
+
+    if (!user) {
+      response.writeHead(400, { 'content-type': 'application/json' });
+      response.end(JSON.stringify({ error: 'user not found' }));
+    } else {
+      response.writeHead(200, { 'content-type': 'application/json' });
+      response.end(JSON.stringify(user));
+    }
   }
 }
